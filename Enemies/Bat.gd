60a5b1559e9c485bde83c0107da697fd624a61f7
kaiser24 <damsog38@gmail.com>
2021-04-06 22:07:38 -0500

    Learning about enemy ai and simple behavior

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,16 +2,39 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeatheffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200 
+
+#Bat state machine
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+var velocity = Vector2.ZERO
+
 var knockback = Vector2.ZERO
+
+var state = IDLE
 
 onready var stats = $Stats
 
-func _ready():
-	print(stats.max_health)
-	print(stats.health)
+
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		WANDER:
+			pass
+		CHASE:
+			pass
+
+func seek_player():
+	pass
 	
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
